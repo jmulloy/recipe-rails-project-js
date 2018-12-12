d493cdb6db15efb2de999b4b76080ebe8bd85d44
github username <github email address>
2018-12-12 16:24:05 -0500

    Fixed all recipes to display proper info

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -5,16 +5,27 @@ $( document ).ready(function(){
 	listenForClickAllRecipes();
 })
 
+class Recipe {
+    constructor(attr) {
+        this.id = attr.id
+        this.name = attr.name
+        this.time = attr.time
+        this.instructions = attr.instructions
+        this.description = attr.description
+        this.ingredients = attr.ingredients
+        this.quantities = attr.quantities
+    }
+}
+
 function listenForClickAllRecipes() {
 	let asdf = document.getElementById('allrecipes')
 	asdf.addEventListener('click', function (e) {
-		e.preventDefault()
-        
+        e.preventDefault()        
 		$.get("/recipes.json", function(data){
             let recipes = data
             let somevariable = ""
             recipes.forEach((recipe, index) => {
-                somevariable += '<li ><a onClick="listenForClickRecipeName()" class="recipename" href="recipes/' + recipe["id"] + '">' + recipe["name"] + `</a>
+                somevariable += '<li ><a class="recipename" href="recipes/' + recipe["id"] + '">' + recipe["name"] + `</a>
              <div id="${index}"` + recipe["description"] + '</div> </li>';
             });
 
@@ -28,16 +39,41 @@ function listenForClickAllRecipes() {
 function listenForClickRecipeName(clicked_id) {
 	$(".recipename").click(function (e) {
         e.preventDefault()
-        $.get("/recipes.json", function(data){
-             let recipes = data
-             let somevariable = ""
-             let linkIndex = document.getElementById('clicked_in')
-              somevariable = '<li>' + recipes[0]["instructions"] + ' - ' + recipes[0]["time"] + '</li>'
-                $("#0").append(somevariable)
+        let url = this.attributes.href.textContent
+
+        $.get(url + ".json", function(data){
+            let recipe = new Recipe(data)            
+            $("#ajax-content").html(recipe.construct())
                 //<li><a class="recipename" href="recipes/1">Soup</a> - Nice meal for when feeling under the weather</li>
         })
     })
 }
+
+Recipe.prototype.construct = function() {
+    let html = `<h2>${this.name}</h2>`
+    html += `<h4>${this.description}</h4><ul>`
+    for (let i = 0; i < this.quantities.length; i ++) {
+        html += `<li>${this.quantities[i].amount} ${this.ingredients[i].name}</li>`
+    }
+    html += `</ul><p>${this.instructions}</p>`
+    return html
+}
+
+    
+    
+    
+    
+    
+    // let somevariable = ""
+
+
+    //          let linkIndex = document.getElementById('clicked_in')
+    //           somevariable = '<li>' + recipes[id]["instructions"] + ' - ' + recipes[id]["time"] + '</li>'
+    //             // $(`#${id}`).html(somevariable)
+//                 html += somevariable 
+//                 html += `</div>`
+// }           
+
 
 
 
@@ -53,15 +89,7 @@ function listenForClickRecipeName(clicked_id) {
 // })
 
 
-class Recipe {
-    constructor(obj) {
-        this.name = obj.name
-        this.time = obj.time
-        this.instructions = obj.instructions
-        this.description = obj.description
-        this.ingredients = obj.ingredients
-    }
-}
+
 
 // function listenForClickMyRecipes() {
 //     let action = document.getElementById('myrecipes')
